e799a3e4c605f46c4a991b7464fc81db7b7f6834
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-25 22:52:18 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,27 @@
-def mario
+
+
+def mario 
+  phrase = "It's-a me, Mario!"
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
+mario
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+toadstool
+
+def link
+  phrase2 = "It's Dangerous To Go Alone! Take This."
+  puts phrase2
+end
+link
+
+def all_phrases
+    mario
+    toadstool
+    link
+end
+all_phrases
